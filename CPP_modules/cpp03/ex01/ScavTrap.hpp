5c277ede43e0a1efb76c23b0e82f9e0caa697f64
kenWR <skyqkqk1168@gmail.com>
2024-10-31 21:48:29 +0900

    🚑 fix ScavTrap

--- CPP_modules/cpp03/ex01/ScavTrap.hpp
+++ CPP_modules/cpp03/ex01/ScavTrap.hpp
@@ -13,14 +13,9 @@ public:
 	~ScavTrap();
 
 	void attack(const std::string& target);
-	void takeDamage(unsigned int amount);
-	void beRepaired(unsigned int amount);
 	void guardGate();
 
 	void printAttack(const std::string& name, const std::string& target, unsigned int damage);
-	void printTakeDamage(const std::string& name, unsigned int damage);
-	void printBeRepaired(const std::string& name, unsigned int damage);
-	void printNeutralized(const std::string& name);
 	void printNotWorking(const std::string& name);
 	void printNotEnoughEnergy(const std::string& name);
 };
--- CPP_modules/cpp03/ex02/ScavTrap.cpp
+++ CPP_modules/cpp03/ex02/ScavTrap.cpp
@@ -48,66 +48,14 @@ void	ScavTrap::attack(const std::string& target) {
 	this->energy_points_--;
 }
 
-void	ScavTrap::takeDamage(unsigned int amount) {
-	if (this->hit_points_ == 0) {
-		printTakeDamage(this->name_, amount);
-		return;
-	}
-	if (amount >= this->hit_points_) {
-		printTakeDamage(this->name_, amount);
-		printNeutralized(this->name_);
-		this->hit_points_ = 0;
-		return;
-	}
-
-	printTakeDamage(this->name_, amount);
-	this->hit_points_ -= amount;
-}
-
-void	ScavTrap::beRepaired(unsigned int amount) {
-	if (this->hit_points_ == 0) {
-		printNotWorking(this->name_);
-		return;
-	}
-	if (this->energy_points_ == 0) {
-		printNotEnoughEnergy(this->name_);
-		return;
-	}
-	if (amount > 10 || this->hit_points_ + amount > 10) {
-		printBeRepaired(this->name_, amount);
-		this->energy_points_--;
-		this->hit_points_ = 10;	
-		return;
-	}
-
-	printBeRepaired(this->name_, amount);
-	this->energy_points_--;
-	this->hit_points_ += amount;
-}
-
-
 void ScavTrap::guardGate() {
 	std::cout << "ScavTrap "  << this->name_ 
 			  << " is now on gate mode! YOU FXXKED UP!" << std::endl;
 }
 
-
 void ScavTrap::printAttack(const std::string& name, const std::string& target, unsigned int damage) {
 	std::cout << "ScavTrap " << (name) << " attacks " << (target) \
               << ", causing " << (damage) << " points of damage!" << std::endl;
-}
-
-void ScavTrap::printTakeDamage(const std::string& name, unsigned int damage) {
-	std::cout << "ScavTrap " << (name) << " take " << (damage) \
-			  << " points of damage!" << std::endl;}
-
-void ScavTrap::printBeRepaired(const std::string& name, unsigned int damage) {
-	std::cout << "ScavTrap " << (name) << " is repaired " << (damage) \
-			  << " points of durability!" << std::endl;
-}
-
-void ScavTrap::printNeutralized(const std::string& name) {
-	std::cout << "ScavTrap " << (name) << " is neutralized!" << std::endl;
 }
 
 void ScavTrap::printNotWorking(const std::string& name) {
