51fd641b51ca7195a716f0915a1c5593c56840bb
kenWR <skyqkqk1168@gmail.com>
2024-11-14 16:43:19 +0900

    ✅ add more test

--- CPP_modules/cpp04/ex03/main.cpp
+++ CPP_modules/cpp04/ex03/main.cpp
@@ -3,6 +3,8 @@
 #include "Ice.hpp"
 #include "Cure.hpp"
 
+#include <iostream>
+
 int main()
 {
 	IMateriaSource* src = new MateriaSource();
@@ -17,11 +19,32 @@ int main()
 	me->equip(tmp);
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
+	tmp = src->createMateria("wrong materia");
+	me->equip(tmp);
 
 	ICharacter* bob = new Character("bob");
+	ICharacter* banchan = new Character("banchan");
 
 	me->use(0, *bob);
 	me->use(1, *bob);
+	me->use(2, *bob); // empty slot
+	me->use(3, *bob); // empty slot
+	me->use(4, *bob); // out of scope
+
+	std::cout << "bob can shoot ice bolt to banchan!" << std::endl;
+	tmp = src->createMateria("ice");
+	bob->equip(tmp);
+	bob->use(0, *banchan);
+
+	std::cout << "and... banchan cure bob!" << std::endl;
+	tmp = src->createMateria("cure");
+	banchan->equip(tmp);
+	banchan->use(0, *bob);
+	banchan->unequip(0); // cure unequip
+	banchan->unequip(1); // nothing
+	banchan->unequip(2); // nothing
+	banchan->unequip(3); // nothing
+	banchan->unequip(4); // out of scope
 
 	delete bob;
 	delete me;
