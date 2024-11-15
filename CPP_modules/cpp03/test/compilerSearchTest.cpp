32188a0b0cce7a0510eb661318c098d059147e77
kenWR <skyqkqk1168@gmail.com>
2024-11-15 23:33:08 +0900

    다중 상속에서 함수 탐색 순서를 설명
    - 탐색 순서는 DiamondTrap → ScavTrap → FragTrap → ClapTrap.
    - 가상 상속 시 중복된 Base 멤버는 하나로 통합.
    - 동일 이름 함수가 여러 Base에 있을 경우 모호성 에러 발생.

---
+++ CPP_modules/cpp03/test/compilerSearchTest.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class ClapTrap { 
+public:
+	void attack() { std::cout << "ClapTrap attack" << std::endl; }
+};
+
+class ScavTrap : virtual public ClapTrap  { 
+public:
+	//void attack() { std::cout << "ScavTrap attack" << std::endl; }
+};
+class FragTrap : virtual public ClapTrap  { 
+public:	
+	void attack() { std::cout << "FragTrap attack" << std::endl; }
+};
+
+class DiamondTrap : public ScavTrap, public FragTrap { 
+public:	
+	// void attack() { std::cout << "DiamondTrap attack" << std::endl; }
+};
+
+int main() {
+	DiamondTrap dt;
+	dt.attack();
+	return 0;
+}
