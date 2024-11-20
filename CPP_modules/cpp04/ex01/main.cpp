80bda847da389ffd04b43e9d909ce325b86efac6
kenWR <skyqkqk1168@gmail.com>
2024-11-20 21:14:29 +0900

    fix: fix typos

--- CPP_modules/cpp04/ex01/main.cpp
+++ CPP_modules/cpp04/ex01/main.cpp
@@ -10,7 +10,7 @@ int main()
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
 	
-	delete j;//should not create a leak
+	delete j; //should not create a leak
 	delete i;
 
 	Animal* animals[10];
