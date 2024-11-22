0674bb478c75abf2b9ef1314d6273220c82a1013
kenWR <skyqkqk1168@gmail.com>
2024-11-22 22:29:42 +0900

    feat: add config file

---
+++ CPP_modules/cpp04/ex03/config.hpp
@@ -0,0 +1,27 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+#define MAX_MATERIA 4
+#define ICE "ice"
+#define CURE "cure"
+
+// ============ ANSI Escape Code ============
+
+#define RED "\033[31m"
+#define BLACK "\033[30m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define WHITE "\033[37m"
+#define GREY "\033[90m"
+
+#define RESET "\033[0m"
+#define BOLD "\033[1m"
+#define DIM "\033[2m"
+#define UNDERLINE "\033[4m"
+#define BLINK "\033[5m"
+
+
+#endif
