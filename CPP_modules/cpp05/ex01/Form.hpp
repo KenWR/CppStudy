263d14873ffd096a48129002cd2cf9c56459cdd4
kenWR <skyqkqk1168@gmail.com>
2025-03-05 10:48:50 +0900

    chore: Formatted by Microsoft style.

--- CPP_modules/cpp05/ex01/Form.hpp
+++ CPP_modules/cpp05/ex01/Form.hpp
@@ -6,42 +6,44 @@
 
 class Bureaucrat;
 
-class Form {
- public:
-  Form(const std::string name, const int requiredGradeToSign,
-       const int requiredGradeToExecute);
-  ~Form();
+class Form
+{
+  public:
+    Form(const std::string name, const int requiredGradeToSign, const int requiredGradeToExecute);
+    ~Form();
 
-  // getters
-  std::string getName() const;
-  int getRequiredGradeToSign() const;
-  int getRequiredGradeToExecute() const;
-  bool getIsSigned() const;
+    // getters
+    std::string getName() const;
+    int getRequiredGradeToSign() const;
+    int getRequiredGradeToExecute() const;
+    bool getIsSigned() const;
 
-  // member functions
-  void beSigned(const Bureaucrat& Bureaucrat);
-  void signForm(const Bureaucrat& Bureaucrat);
+    // member functions
+    void beSigned(const Bureaucrat &Bureaucrat);
+    void signForm(const Bureaucrat &Bureaucrat);
 
-  class GradeTooHighException : public std::exception {
-   public:
-    virtual const char* what() const throw();
-  };
+    class GradeTooHighException : public std::exception
+    {
+      public:
+        virtual const char *what() const throw();
+    };
 
-  class GradeTooLowException : public std::exception {
-   public:
-    virtual const char* what() const throw();
-  };
+    class GradeTooLowException : public std::exception
+    {
+      public:
+        virtual const char *what() const throw();
+    };
 
- private:
-  const std::string name_;
-  const int requiredGradeToSign_;
-  const int requiredGradeToExecute_;
-  bool isSigned_;
+  private:
+    const std::string name_;
+    const int requiredGradeToSign_;
+    const int requiredGradeToExecute_;
+    bool isSigned_;
 
-  Form(const Form& rhs);
-  Form& operator=(const Form& rhs);
+    Form(const Form &rhs);
+    Form &operator=(const Form &rhs);
 };
 
-std::ostream& operator<<(std::ostream& o, const Form& rhs);
+std::ostream &operator<<(std::ostream &o, const Form &rhs);
 
 #endif
