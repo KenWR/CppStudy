b278c5ed0e6fe48afa320647f93a2d3532bb398c
kenWR <skyqkqk1168@gmail.com>
2025-03-05 10:38:14 +0900

    chore: change format.

--- CPP_modules/cpp05/ex00/Bureaucrat.hpp
+++ CPP_modules/cpp05/ex00/Bureaucrat.hpp
@@ -3,44 +3,47 @@
 
 #include <string>
 
-class Bureaucrat {
- public:
-  static const int kHighestGrade_ = 1;
-  static const int kLowestGrade_ = 150;
+class Bureaucrat
+{
+  public:
+    static const int kHighestGrade_ = 1;
+    static const int kLowestGrade_ = 150;
 
-  Bureaucrat(const std::string& name, const int grade);
-  ~Bureaucrat();
+    Bureaucrat(const std::string &name, const int grade);
+    ~Bureaucrat();
 
-  // getters
-  const std::string& getName() const;
-  int getGrade() const;
+    // getters
+    const std::string &getName() const;
+    int getGrade() const;
 
-  // setters
-  void setGrade(const int grade);
+    // setters
+    void setGrade(const int grade);
 
-  // member functions
-  void incrementGrade();
-  void decrementGrade();
-  void validateGrade(int grade) const;
+    // member functions
+    void incrementGrade();
+    void decrementGrade();
+    void validateGrade(int grade) const;
 
-  // exceptions
-  class GradeTooHighException : public std::exception {
-   public:
-    virtual const char* what() const throw();
-  };
-  class GradeTooLowException : public std::exception {
-   public:
-    virtual const char* what() const throw();
-  };
+    // exceptions
+    class GradeTooHighException : public std::exception
+    {
+      public:
+        virtual const char *what() const throw();
+    };
+    class GradeTooLowException : public std::exception
+    {
+      public:
+        virtual const char *what() const throw();
+    };
 
- private:
-  const std::string name_;
-  int grade_;  // 1 to 150, 1 is the highest grade, 150 is the lowest
+  private:
+    const std::string name_;
+    int grade_; // 1 to 150, 1 is the highest grade, 150 is the lowest
 
-  Bureaucrat(const Bureaucrat& rhs);
-  Bureaucrat& operator=(const Bureaucrat& rhs);
+    Bureaucrat(const Bureaucrat &rhs);
+    Bureaucrat &operator=(const Bureaucrat &rhs);
 };
 
-std::ostream& operator<<(std::ostream& o, const Bureaucrat& rhs);
+std::ostream &operator<<(std::ostream &o, const Bureaucrat &rhs);
 
 #endif
