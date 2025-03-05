4e4be311370f183aa41d12f07b2f1c86cd9e5a00
kenWR <skyqkqk1168@gmail.com>
2025-03-05 10:47:58 +0900

    fix: Change files to fixed Bureaucrat.

--- CPP_modules/cpp05/ex01/Bureaucrat.cpp
+++ CPP_modules/cpp05/ex01/Bureaucrat.cpp
@@ -2,68 +2,79 @@
 
 #include <iostream>
 
-#include "Form.hpp"
+#include "Config.hpp"
 
-Bureaucrat::Bureaucrat(const std::string& name, const int grade)
-    : name_(name), grade_(grade) {
-  validateGrade(grade_);
+Bureaucrat::Bureaucrat(const std::string &name, const int grade) : name_(name), grade_(grade)
+{
+    validateGrade(grade_);
+    std::cout << O_YELLOW << "Created Bureaucrats successfully\n" << O_RESET;
 }
 
-Bureaucrat::~Bureaucrat() {}
-
-const std::string& Bureaucrat::getName() const { return name_; }
-
-int Bureaucrat::getGrade() const { return grade_; }
-
-void Bureaucrat::setGrade(const int grade) {
-  validateGrade(grade);
-
-  grade_ = grade;
+Bureaucrat::~Bureaucrat()
+{
 }
 
-void Bureaucrat::incrementGrade() {
-  if (grade_ == kHighestGrade_) {
-    throw GradeTooHighException();
-  }
-
-  grade_--;
+const std::string &Bureaucrat::getName() const
+{
+    return name_;
 }
 
-void Bureaucrat::decrementGrade() {
-  if (grade_ == kLowestGrade_) {
-    throw GradeTooLowException();
-  }
-
-  grade_++;
+int Bureaucrat::getGrade() const
+{
+    return grade_;
 }
 
-void Bureaucrat::validateGrade(int grade) const {
-  if (grade < kHighestGrade_) {
-    throw GradeTooHighException();
-  } else if (grade > kLowestGrade_) {
-    throw GradeTooLowException();
-  }
+void Bureaucrat::setGrade(const int grade)
+{
+    validateGrade(grade);
+
+    grade_ = grade;
 }
 
-void Bureaucrat::signForm(class Form& form) {
-  if (form.getIsSigned()) {
-    std::cout << name_ << " couldn't sign " << form.getName()
-              << " because the form is already signed" << std::endl;
-  } else {
-    std::cout << name_ << " signen " << form.getName() << std::endl;
-    form.beSigned(*this);
-  }
+void Bureaucrat::incrementGrade()
+{
+    if (grade_ == kHighestGrade_)
+    {
+        throw GradeTooHighException();
+    }
+
+    grade_--;
 }
 
-const char* Bureaucrat::GradeTooHighException::what() const throw() {
-  return "Grade is too high";
+void Bureaucrat::decrementGrade()
+{
+    if (grade_ == kLowestGrade_)
+    {
+        throw GradeTooLowException();
+    }
+
+    grade_++;
 }
 
-const char* Bureaucrat::GradeTooLowException::what() const throw() {
-  return "Grade is too low";
+void Bureaucrat::validateGrade(int grade) const
+{
+    if (grade < kHighestGrade_)
+    {
+        throw GradeTooHighException();
+    }
+    else if (grade > kLowestGrade_)
+    {
+        throw GradeTooLowException();
+    }
 }
 
-std::ostream& operator<<(std::ostream& o, const Bureaucrat& rhs) {
-  o << rhs.getName() << ", bureaucrat grade " << rhs.getGrade();
-  return o;
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return "Grade is too high";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return "Grade is too low";
+}
+
+std::ostream &operator<<(std::ostream &o, const Bureaucrat &rhs)
+{
+    o << rhs.getName() << ", bureaucrat grade " << rhs.getGrade();
+    return o;
 }
