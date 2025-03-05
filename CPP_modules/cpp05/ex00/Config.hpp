bb1a27660d4c88bd11bffde7e1a1fdb320cf9a2b
kenWR <skyqkqk1168@gmail.com>
2025-03-05 10:11:47 +0900

    chore: Config file macros renamed.

--- CPP_modules/cpp05/ex00/Config.hpp
+++ CPP_modules/cpp05/ex00/Config.hpp
@@ -3,19 +3,19 @@
 
 // ============ ANSI Escape Code ============
 
-#define RED "\033[31m"
-#define BLACK "\033[30m"
-#define GREEN "\033[38;5;22m"
-#define YELLOW "\033[33m"
-#define BLUE "\033[34m"
-#define CYAN "\033[36m"
-#define MAGENTA "\033[35m"
-#define BROWN "\033[38;5;214m"
-#define GREY "\033[90m"
+#define OUTPUT_RED "\033[31m"
+#define OUTPUT_BLACK "\033[30m"
+#define OUTPUT_GREEN "\033[38;5;22m"
+#define OUTPUT_YELLOW "\033[33m"
+#define OUTPUT_BLUE "\033[34m"
+#define OUTPUT_CYAN "\033[36m"
+#define OUTPUT_MAGENTA "\033[35m"
+#define OUTPUT_BROWN "\033[38;5;214m"
+#define OUTPUT_GREY "\033[90m"
 
-#define RESET "\033[0m"
-#define BOLD "\033[1m"
-#define DIM "\033[2m"
-#define UNDERLINE "\033[4m"
+#define OUTPUT_RESET "\033[0m"
+#define OUTPUT_BOLD "\033[1m"
+#define OUTPUT_DIM "\033[2m"
+#define OUTPUT_UNDERLINE "\033[4m"
 
 #endif
