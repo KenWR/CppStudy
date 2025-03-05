38cd7de35214c271097c14168751841f01c71a62
kenWR <skyqkqk1168@gmail.com>
2025-03-05 09:58:48 +0900

    feat: Config.hpp added for colored output.

---
+++ CPP_modules/cpp05/ex00/Config.hpp
@@ -0,0 +1,21 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+// ============ ANSI Escape Code ============
+
+#define RED "\033[31m"
+#define BLACK "\033[30m"
+#define GREEN "\033[38;5;22m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define CYAN "\033[36m"
+#define MAGENTA "\033[35m"
+#define BROWN "\033[38;5;214m"
+#define GREY "\033[90m"
+
+#define RESET "\033[0m"
+#define BOLD "\033[1m"
+#define DIM "\033[2m"
+#define UNDERLINE "\033[4m"
+
+#endif
