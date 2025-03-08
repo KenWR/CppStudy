d2d0dfd73a1cc8bdcb567d49de59c2cab79270d1
kenWR <skyqkqk1168@gmail.com>
2025-03-08 18:31:22 +0900

    feat: main.cpp added.

---
+++ CPP_modules/cpp05/ex02/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "Bureaucrat.hpp"
+#include "Config.hpp"
+
+int main() {
+  std::cout << O_BOLD << "=========== CPP MODULE 05 / EX 00 ===========\n\n"
+            << O_RESET;
+
+}
