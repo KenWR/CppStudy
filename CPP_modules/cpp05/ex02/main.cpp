71d70abedb3d9b10159f3b245b8a918a90bad830
kenWR <skyqkqk1168@gmail.com>
2025-03-09 23:54:03 +0900

    feat: The form objects are declared by pointers;

--- CPP_modules/cpp05/ex02/main.cpp
+++ CPP_modules/cpp05/ex02/main.cpp
@@ -11,6 +11,10 @@ int main() {
   std::cout << O_BOLD << "=========== CPP MODULE 05 / EX 00 ===========\n\n"
             << O_RESET;
 
+  AForm *shrubbery_creation = NULL;
+  AForm *robotomy_request = NULL;
+  AForm *presidential_pardon = NULL;
+
   try {
     std::cout << O_BOLD << "============= Bureaucrats =============\n"
               << O_RESET;
@@ -21,35 +25,41 @@ int main() {
               << O_RESET;
 
     std::cout << O_BOLD << "============= Forms =============\n" << O_RESET;
-    ShrubberyCreationForm shrubbery_creation("home");
-    RobotomyRequestForm robotomy_request("Jack");
-    PresidentialPardonForm presidential_pardon("Jack");
+
+    shrubbery_creation = new ShrubberyCreationForm("home");
+    robotomy_request = new RobotomyRequestForm("Jack");
+    presidential_pardon = new PresidentialPardonForm("Jack");
     std::cout << O_BOLD << "========================================\n"
               << O_RESET;
 
     std::cout << O_BOLD << "============= Not signed =============\n"
               << O_RESET;
-    trump.executeForm(shrubbery_creation);
-    elon.executeForm(robotomy_request);
-    bob.executeForm(presidential_pardon);
+    trump.executeForm(*shrubbery_creation);
+    elon.executeForm(*robotomy_request);
+    bob.executeForm(*presidential_pardon);
     std::cout << O_BOLD << "========================================\n"
               << O_RESET;
 
     std::cout << O_BOLD << "============= Sign form =============\n" << O_RESET;
-    trump.signForm(shrubbery_creation);
-    elon.signForm(robotomy_request);
-    bob.signForm(presidential_pardon);
+    trump.signForm(*shrubbery_creation);
+    elon.signForm(*robotomy_request);
+    bob.signForm(*presidential_pardon);
     std::cout << O_BOLD << "========================================\n"
               << O_RESET;
 
-    std::cout << O_BOLD << "============= Execute form =============\n" << O_RESET;
-    trump.executeForm(shrubbery_creation);
-    elon.executeForm(robotomy_request);
-    bob.executeForm(presidential_pardon);
+    std::cout << O_BOLD << "============= Execute form =============\n"
+              << O_RESET;
+    trump.executeForm(*shrubbery_creation);
+    elon.executeForm(*robotomy_request);
+    bob.executeForm(*presidential_pardon);
     std::cout << O_BOLD << "========================================\n"
               << O_RESET;
 
   } catch (std::exception &e) {
     std::cerr << e.what() << '\n';
   }
+
+  delete shrubbery_creation;
+  delete robotomy_request;
+  delete presidential_pardon;
 }
