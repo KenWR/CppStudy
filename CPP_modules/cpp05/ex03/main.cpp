b2fdfeaa10aab6854994b86455530882043c1349
KenWR <skyqkqk1168@gmail.com>
2025-03-10 14:49:13 +0900

    feat: main.cpp added with test cases.

--- CPP_modules/cpp05/ex03/main.cpp
+++ CPP_modules/cpp05/ex03/main.cpp
@@ -3,6 +3,7 @@
 #include "AForm.hpp"
 #include "Bureaucrat.hpp"
 #include "Config.hpp"
+#include "Intern.hpp"
 #include "PresidentialPardonForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "ShrubberyCreationForm.hpp"
@@ -11,45 +12,55 @@ int main() {
   std::cout << O_BOLD << "=========== CPP MODULE 05 / EX 00 ===========\n\n"
             << O_RESET;
 
+  AForm *shrubberyCreation = NULL;
+  AForm *robotomyRequest = NULL;
+  AForm *presidentialPardon = NULL;
+  AForm *wrongForm = NULL;
+
   try {
-    std::cout << O_BOLD << "============= Bureaucrats =============\n"
+    std::cout << O_BOLD << "============= Bureaucrat, Intern =============\n"
               << O_RESET;
     Bureaucrat trump("President", 1);
-    Bureaucrat elon("minister", 30);
-    Bureaucrat bob("clerk", 150);
+    Intern intern;
+
     std::cout << O_BOLD << "========================================\n"
               << O_RESET;
 
     std::cout << O_BOLD << "============= Forms =============\n" << O_RESET;
-    ShrubberyCreationForm shrubbery_creation("home");
-    RobotomyRequestForm robotomy_request("Jack");
-    PresidentialPardonForm presidential_pardon("Jack");
-    std::cout << O_BOLD << "========================================\n"
-              << O_RESET;
 
-    std::cout << O_BOLD << "============= Not signed =============\n"
-              << O_RESET;
-    trump.executeForm(shrubbery_creation);
-    elon.executeForm(robotomy_request);
-    bob.executeForm(presidential_pardon);
+    shrubberyCreation = intern.makeForm("ShrubberyCreationForm", "home");
+    robotomyRequest = intern.makeForm("RobotomyRequestForm", "Jack");
+    presidentialPardon = intern.makeForm("PresidentialPardonForm", "Jack");
     std::cout << O_BOLD << "========================================\n"
               << O_RESET;
 
     std::cout << O_BOLD << "============= Sign form =============\n" << O_RESET;
-    trump.signForm(shrubbery_creation);
-    elon.signForm(robotomy_request);
-    bob.signForm(presidential_pardon);
+    trump.signForm(*shrubberyCreation);
+    trump.signForm(*robotomyRequest);
+    trump.signForm(*presidentialPardon);
     std::cout << O_BOLD << "========================================\n"
               << O_RESET;
 
-    std::cout << O_BOLD << "============= Execute form =============\n" << O_RESET;
-    trump.executeForm(shrubbery_creation);
-    elon.executeForm(robotomy_request);
-    bob.executeForm(presidential_pardon);
+    std::cout << O_BOLD << "============= Execute form =============\n"
+              << O_RESET;
+    trump.executeForm(*shrubberyCreation);
+    trump.executeForm(*robotomyRequest);
+    trump.executeForm(*presidentialPardon);
+    std::cout << O_BOLD << "========================================\n"
+              << O_RESET;
+
+    std::cout << O_BOLD << "============= Create wrong form =============\n"
+              << O_RESET;
+    wrongForm = intern.makeForm("worng name", "Jack");
     std::cout << O_BOLD << "========================================\n"
               << O_RESET;
 
   } catch (std::exception &e) {
     std::cerr << e.what() << '\n';
   }
+
+  delete shrubberyCreation;
+  delete robotomyRequest;
+  delete presidentialPardon;
+  delete wrongForm;
 }
