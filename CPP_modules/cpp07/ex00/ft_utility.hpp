9f926d8da962d26260927e5dbe45724c46fce9f1
KenWR <skyqkqk1168@gmail.com>
2025-03-13 17:55:58 +0900

    style: annotation style changed.

--- CPP_modules/cpp07/ex00/ft_utility.hpp
+++ CPP_modules/cpp07/ex00/ft_utility.hpp
@@ -2,6 +2,7 @@
 #define FT_UTILITY_HPP
 
 #include <iostream>
+#include <string>
 
 namespace ft {
 
@@ -20,39 +21,39 @@ template <typename T> T max(const T &a, const T &b) { return (a > b) ? a : b; }
  * function without explicitly specifying template parameters (<>), the compiler
  * will prioritize non-template functions over template functions if both are
  * viable candidates for the function call.
- *
- * void swap(int a, int b) { // this swap is wrong.
- *   int c = a + b;
- *   (void)c;
- *   std::cout << "Wrong swap called!\n";
- * };
- *
- * int min(int a, int b) { // this min is wrong.
- *   std::cout << "Wrong min called!\n";
- *   return (a > b) ? a : b;
- * }
- *
- * int max(int a, int b) { // this max is wrong.
- *   std::cout << "Wrong max called!\n";
- *   return (a < b) ? a : b;
- * }
- *
- * void swap(std::string a, std::string b) { // this swap is wrong.
- *   std::string c = a + b;
- *   (void)c;
- *   std::cout << "Wrong swap called!\n";
- * };
- *
- * std::string min(std::string a, std::string b) { // this min is wrong.
- *   std::cout << "Wrong min called!\n";
- *   return (a > b) ? a : b;
- * }
- *
- * std::string max(std::string a, std::string b) { // this max is wrong.
- *   std::cout << "Wrong max called!\n";
- *   return (a < b) ? a : b;
- * }
  */
+
+// void swap(int a, int b) { // this swap is wrong.
+//   int c = a + b;
+//   (void)c;
+//   std::cout << "Wrong swap called!\n";
+// };
+
+// int min(int a, int b) { // this min is wrong.
+//   std::cout << "Wrong min called!\n";
+//   return (a > b) ? a : b;
+// }
+
+// int max(int a, int b) { // this max is wrong.
+//   std::cout << "Wrong max called!\n";
+//   return (a < b) ? a : b;
+// }
+
+// void swap(std::string a, std::string b) { // this swap is wrong.
+//   std::string c = a + b;
+//   (void)c;
+//   std::cout << "Wrong swap called!\n";
+// };
+
+// std::string min(std::string a, std::string b) { // this min is wrong.
+//   std::cout << "Wrong min called!\n";
+//   return (a > b) ? a : b;
+// }
+
+// std::string max(std::string a, std::string b) { // this max is wrong.
+//   std::cout << "Wrong max called!\n";
+//   return (a < b) ? a : b;
+// }
 
 } // namespace ft
 
